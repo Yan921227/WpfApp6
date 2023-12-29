cb94b882903d432d2e8f2918b191e1c3a9ae59d2
Yan921227 <125242772+Yan921227@users.noreply.github.com>
2023-12-29 13:52:48 +0800

    讀取網路資料，填入DataGrid產生動態

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
+using System.Windows.Data; 
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
